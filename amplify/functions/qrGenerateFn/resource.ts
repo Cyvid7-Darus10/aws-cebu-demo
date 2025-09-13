bf34ef00a7ea26cf2dad0ac24d404df057c8d081
Cyrus <cyrus@pastelero.ph>
2025-09-13 09:54:17 +0800

    chore: Update BASE_URL to use secret management in QR generation function
    - Changed the BASE_URL in `resource.ts` to utilize the secret management feature for enhanced security.

--- amplify/functions/qrGenerateFn/resource.ts
+++ amplify/functions/qrGenerateFn/resource.ts
@@ -1,9 +1,9 @@
-import { defineFunction } from "@aws-amplify/backend";
+import { defineFunction, secret } from "@aws-amplify/backend";
 
 export const qrGenerateFn = defineFunction({
   name: "qr-generate-fn",
   entry: "./handler.ts",
   environment: {
-    BASE_URL: "https://main.d2uj1ffub6eiln.amplifyapp.com",
+    BASE_URL: secret("BASE_URL"),
   },
 });
